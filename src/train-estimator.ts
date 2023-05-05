454489e60177bd81db24a7fb2e0f43e779c96133
Flavian Engevin <flavian.engevin@ynov.com>
2023-05-05 12:13:43 +0200

    fix: bug

--- src/train-estimator.ts
+++ src/train-estimator.ts
@@ -20,7 +20,7 @@ export class TrainTicketEstimator {
         }
 
         // TODO USE THIS LINE AT THE END
-        const b = (await(await fetch(`https://sncf.com/api/train/estimate/price?from=${trainDetails.details.from}&to=${trainDetails.details.to}&date=${trainDetails.details.when}`)).json())?.price || -1;
+        const b = (await (await fetch(`https://sncf.com/api/train/estimate/price?from=${trainDetails.details.from}&to=${trainDetails.details.to}&date=${trainDetails.details.when}`)).json())?.price || -1;
 
         if (b === -1) {
             throw new ApiException();
@@ -29,35 +29,35 @@ export class TrainTicketEstimator {
         const pasngers = trainDetails.passengers;
         let tot = 0;
         let tmp = b;
-        for (let i=0;i<pasngers.length;i++) {
+        for (let i = 0; i < pasngers.length; i++) {
 
             if (pasngers[i].age < 0) {
                 throw new InvalidTripInputException("Age is invalid");
             }
             if (pasngers[i].age < 1) {
-                tmp = 0;
+                continue;
             }
             // Seniors
             else if (pasngers[i].age <= 17) {
-            tmp = b* 0.6;
-            } else if(pasngers[i].age >= 70) {
+                tmp = b * 0.6;
+            } else if (pasngers[i].age >= 70) {
                 tmp = b * 0.8;
                 if (pasngers[i].discounts.includes(DiscountCard.Senior)) {
                     tmp -= b * 0.2;
                 }
             } else {
-                tmp = b*1.2;
+                tmp = b * 1.2;
             }
 
             const d = new Date();
-            if (trainDetails.details.when.getTime() >= d.setDate(d.getDate() +30)) {
+            if (trainDetails.details.when.getTime() >= d.setDate(d.getDate() + 30)) {
                 tmp -= b * 0.2;
-            } else if (trainDetails.details.when.getTime() > d.setDate(d.getDate() -30 + 5)) {
+            } else if (trainDetails.details.when.getTime() > d.setDate(d.getDate() - 30 + 5)) {
                 const date1 = trainDetails.details.when;
                 const date2 = new Date();
                 //https://stackoverflow.com/questions/43735678/typescript-get-difference-between-two-dates-in-days
-                var diff = Math.abs(date1.getTime() - date2.getTime());
-                var diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+                const diff = Math.abs(date1.getTime() - date2.getTime());
+                const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
 
                 tmp += (20 - diffDays) * 0.02 * b; // I tried. it works. I don't know why.
             } else {
@@ -79,7 +79,7 @@ export class TrainTicketEstimator {
         if (pasngers.length == 2) {
             let cp = false;
             let mn = false;
-            for (let i=0;i<pasngers.length;i++) {
+            for (let i = 0; i < pasngers.length; i++) {
                 if (pasngers[i].discounts.includes(DiscountCard.Couple)) {
                     cp = true;
                 }
@@ -95,7 +95,7 @@ export class TrainTicketEstimator {
         if (pasngers.length == 1) {
             let cp = false;
             let mn = false;
-            for (let i=0;i<pasngers.length;i++) {
+            for (let i = 0; i < pasngers.length; i++) {
                 if (pasngers[i].discounts.includes(DiscountCard.HalfCouple)) {
                     cp = true;
                 }
